9dfa420a06feab5cf210eb227c989234a1822968
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2022-02-11 16:42:05 -0800

    content : a bit of whimsy

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // const closeModal = document.getElementsByClassName('project-modal');
 // console.log(closeModal);
+
+window.onfocus = function() {
+  document.title = "Portfolio : Yay you're here";
+}
+
+window.onblur = function() {
+  document.title = "Portfolio : Aw you left";
+}
