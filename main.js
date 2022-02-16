7c3120fcfd911fd50a2b6499faabd5a8f092d763
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2022-02-15 16:29:40 -0800

    content : display astronaut dialogue screen

--- main.js
+++ main.js
@@ -11,8 +11,23 @@ window.onblur = function() {
 // ASTRONAUT ANIMATION
 
 const astronaut = document.getElementById('astronaut');
+const astronautText = document.getElementById('astronaut-dialogue-screen');
 
 astronaut.addEventListener('click',astronautDialogue);
+
+function astronautDialogue() {
+  astronautText.textContent = 'hi';
+  toggleDialogueScreen();
+}
+
+function toggleDialogueScreen() {
+  if (astronautText.style.display === "none") {
+    astronautText.style.display = "block";
+  }
+  else {
+    astronautText.style.display = "none";
+  }
+}
 
 // ROCKET ANIMATION
 
