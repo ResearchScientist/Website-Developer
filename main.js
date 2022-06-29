e967df36a99c3e09c1e5d2cdfd2d516e917588dd
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2022-06-29 15:49:57 -0700

    content : add ufo to telescope view

--- main.js
+++ main.js
@@ -175,17 +175,6 @@ const throttleSatellite = (fun,delay) => {
 
 canvasSatellite.addEventListener('click',throttleSatellite(rotateSatellite,2000));
 
-// UFO
-
-const ufo = document.getElementById('ufo');
-const beam = document.getElementById('beam');
-
-function beaming() {
-  beam.classList.toggle('beam');
-}
-
-ufo.addEventListener('click',beaming);
-
 // TRAJECTORY LIGHTS
 
 const trajectory = document.getElementById('trajectory-items');
