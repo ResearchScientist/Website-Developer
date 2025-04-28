2f4c7a83464d58d62ce065dc9543e931f86ce251
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2025-04-28 15:55:32 -0700

    [ux] satellite animation : refactor throttle function to conditional function

--- js/satellite.js
+++ js/satellite.js
@@ -5,48 +5,71 @@ const canvasSatellite = document.getElementById('canvas-satellite');
 const ctx = canvasSatellite.getContext('2d');
 const canvasSatellite_width = canvasSatellite.width = 460;
 const canvasSatellite_height = canvasSatellite.height = 460;
-const canvasSatellite_spritesheet = new Image();
 const canvasSatellite_spriteframe = 460;
 const canvasSatellite_frameSpeed = 2;
 let canvasSatellite_frameX = 0;
 let canvasSatellite_frameY = 0;
 let canvasSatellite_currentFrame = 0;
+let canvasSatellite_loaded = false;
 let canvasSatellite_rotating = false;
+let canvasSatellite_spritesheet;
+let requestID;
 
-// document.addEventListener('DOMContentLoaded',prepSatellite);
+function prepSatellite() {
+  if (!canvasSatellite_loaded) {
+    canvasSatellite_spritesheet = new Image();
+    canvasSatellite_spritesheet.src = 'img/satellite-spritesheet.png';
+    canvasSatellite_spritesheet.onload = function () {
+      canvasSatellite_loaded = true;
+      console.log('satellite sprite loaded');
+    };
+    canvasSatellite_spritesheet.onerror = function () {
+      console.error('error loading satellite sprite');
+    };
+  } else {
+    console.log('satellite sprite already loaded');
+  }
+}
 
-canvasSatellite_spritesheet.src = 'img/satellite-spritesheet.png';
-
-// function prepSatellite() {
-//   canvasSatellite_spritesheet.src = 'img/satellite-spritesheet.png';
-  console.log('satellite prepped');
-// }
+function satelliteFlightCheck() {
+  if (canvasSatellite_loaded && !canvasSatellite_rotating) {
+    rotateSatellite();
+    canvasSatellite_rotating = true;
+    console.log('all systems go : begin rotation');
+  } else if (!canvasSatellite_loaded) {
+    console.log('no go : missing sprite');
+  } else {
+    console.log('rotation in progress');
+  }
+}
 
 function rotateSatellite() {
-  // let requestID;
   imgSatellite.style.visibility = 'hidden';
   ctx.clearRect(0,0,canvasSatellite_width,canvasSatellite_height);
   ctx.drawImage(canvasSatellite_spritesheet,canvasSatellite_frameX * canvasSatellite_spriteframe,canvasSatellite_frameY * canvasSatellite_spriteframe,canvasSatellite_spriteframe,canvasSatellite_spriteframe,0,0,canvasSatellite_spriteframe,canvasSatellite_spriteframe);
   
   if (canvasSatellite_currentFrame % canvasSatellite_frameSpeed == 0) {
-    if (canvasSatellite_frameX < 59) canvasSatellite_frameX++;
+    if (canvasSatellite_frameX < 59)
+      canvasSatellite_frameX++;
     else {
       canvasSatellite_frameX = 0;
+      canvasSatellite_rotating = false;
       cancelAnimationFrame(requestID);
+      return;
     }
   }
   canvasSatellite_currentFrame++;
   requestAnimationFrame(rotateSatellite);
 };
 
-const throttleSatellite = (fun,delay) => {
-  let priorTime = 0;
-  return (...args) => {
-    const currentTime = new Date().getTime();
-    if (currentTime - priorTime < delay) return;
-    priorTime = currentTime;
-    fun(...args);
-  }
-}
+// const throttleSatellite = (fun,delay) => {
+//   let priorTime = 0;
+//   return (...args) => {
+//     const currentTime = new Date().getTime();
+//     if (currentTime - priorTime < delay) return;
+//     priorTime = currentTime;
+//     fun(...args);
+//   }
+// }
 
-canvasSatellite.addEventListener('click',throttleSatellite(rotateSatellite,2000));
+canvasSatellite.addEventListener('click',satelliteFlightCheck);
