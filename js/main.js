a59d92ecd133545df2b79ba5236825e915cd457f
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2025-05-23 13:16:23 -0700

    [ux] gauge : add dial animation

--- js/main.js
+++ js/main.js
@@ -1051,11 +1051,20 @@ function resetEngine() {
 
 // GAUGE
 
+const gaugeDialIMG = document.querySelector('#gauge-dial-img');
+
 function gaugeIntoTheAbyss() {
   console.log('gauging this');
+  gaugeDialIMG.classList.add('gauge-dial-spin');
+  updateInsetButtons('gaugeButton');
 }
 
 function resetGauge() {
+  gaugeDialIMG.classList.remove('gauge-dial-spin');
+  gaugeDialIMG.classList.add('gauge-dial-unspin');
+  setTimeout(() => {
+    gaugeDialIMG.classList.remove('gauge-dial-unspin');
+  }, 1000);
   resetInsetButtons();
 }
 
