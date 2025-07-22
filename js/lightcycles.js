2d80a48cc386319bd389470e85778bb8ac93fc24
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2025-07-21 19:03:12 -0700

    [logic] lightcycles : add start condition, loop, user update, render, collision checks

--- js/lightcycles.js
+++ js/lightcycles.js
@@ -85,6 +85,20 @@ export class LightCyclesGame {
       lightcycle.trail = [{x: lightcycle.x, y: lightcycle.y}];
       this.grid[lightcycle.y][lightcycle.x] = lightcycle;
     });
+
+    this.npcInterval = this.changeNPCspeed();
+  }
+
+  changeNPCspeed() {
+    switch (this.currentRound) {
+      case 1: return 180;
+      case 2: return 160;
+      case 3: return 140;
+      case 4: return 120;
+      case 5: return 100;
+      case 6: return 80;
+      default: return 60;
+    }
   }
 
   bindDirectionKeys() {
@@ -111,9 +125,19 @@ export class LightCyclesGame {
     });
   }
 
+  isValidDirection(currentDir,nextDir) {
+    const oppositeDirections = {
+      'up': 'down',
+      'down': 'up',
+      'left': 'right',
+      'right': 'left'
+    };
+    return oppositeDirections[currentDir] !== nextDir;
+  }
+
   bindSpeedKeys() {
     document.addEventListener('keydown', (e) => {
-      const speedKey = e.speedKey.toLowerCase();
+      const speedKey = e.key.toLowerCase();
       if (speedKey === 's') {
         this.speedKeysHeld.s = true;
         this.updateSpeed();
@@ -124,7 +148,7 @@ export class LightCyclesGame {
       }
     });
     document.addEventListener('keyup', (e) => {
-      const speedKey = e.speedKey.toLowerCase();
+      const speedKey = e.key.toLowerCase();
       if (speedKey === 's') {
         this.speedKeysHeld.s = false;
         this.updateSpeed();
@@ -136,24 +160,83 @@ export class LightCyclesGame {
     });
   }
 
+  updateSpeed() {
+    if (this.speedKeysHeld.f && this.speedKeysHeld.s) {
+      this.userInterval = this.baseInterval;
+    } else if (this.speedKeysHeld.f) {
+      this.userInterval = this.fastInterval;
+    } else if (this.speedKeysHeld.s) {
+      this.userInterval = this.slowInterval;
+    } else {
+      this.userInterval = this.baseInterval;
+    }
+  }
+
   startGame() {
     this.lightcyclesInfo.style.opacity = 0;
     this.gameRunning = true;
     this.init();
+    this.userPrevUpTime = 0;
+    this.npcPrevUpTime = 0;
     this.gameLoop();
     console.log(`is game running : ${this.gameRunning}`);
   }
 
   gameLoop(currentTime = 0) {
     if (!this.gameRunning) return;
-
-
-
+    if (currentTime - this.userPrevUpTime >= this.userInterval) {
+      this.updateUser();
+      this.userPrevUpTime = currentTime;
+    }
+    if (currentTime - this.npcPrevUpTime >= this.npcInterval) {
+      // this.updateNPCs();
+      this.npcPrevUpTime = currentTime;
+    }
     this.render();
-
     this.animationId = requestAnimationFrame((time) => this.gameLoop(time));
   }
 
+  stopGameLoop() {
+    if (this.animationId) {
+      cancelAnimationFrame(this.animationId);
+      this.animationId = null;
+    }
+  }
+
+  updateUser() {
+    if (!this.user.alive) return;
+    const newPos = this.getNewPosition(this.user);
+    if (this.checkCollision(newPos.x, newPos.y)) {
+      this.user.alive = false;
+      // this.checkGameEnd();
+      return;
+    }
+    this.user.x = newPos.x;
+    this.user.y = newPos.y;
+    this.user.trail.push({x: this.user.x, y: this.user.y});
+    this.grid[this.user.y][this.user.x] = this.user;
+  }
+
+  getNewPosition(lightcycle) {
+    const directions = {
+      'up': {x: 0, y: -1},
+      'down': {x: 0, y: 1},
+      'left': {x: -1, y: 0},
+      'right': {x: 1, y: 0}
+    };
+    const dir = directions[lightcycle.direction];
+    return {
+      x: lightcycle.x + dir.x,
+      y: lightcycle.y + dir.y
+    };
+  }
+
+  checkCollision(x,y) {
+    if (x < 0 || x >= this.gridWidth || y < 0 || y >= this.gridHeight) {
+      return true;
+    }
+    return this.grid[y][x] !== 0;
+  }
 
   render() {
     this.ctx.fillStyle = 'rgba(0,0,0,0)';
